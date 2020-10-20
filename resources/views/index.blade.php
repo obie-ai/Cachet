935ec1dff67863b0010c524e132d6039c8b3eeff
Ubuntu <alex@sopinka.com>
2020-10-20 22:37:31 +0000

    Push maintenance notice to the top

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 @include('partials.about-app')
+@include('partials.modules.scheduled')
 @include('partials.modules.messages')
 @include('partials.modules.status')
 @include('partials.modules.components')
 @include('partials.modules.metrics')
 @include('partials.modules.stickied')
-@include('partials.modules.scheduled')
 @include('partials.modules.timeline')
 @stop
 
